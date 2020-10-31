4508f0fea8740de675db1b70315b1f062e6b163f
Rishant <rishant.dutt96@gmail.com>
2020-10-30 23:18:34 -0400

    insert data endpoint working

--- server.js
+++ server.js
@@ -5,18 +5,20 @@ const cors = require("cors");
 const app = express();
 const port = 3200;
 
+var bodyParser = require("body-parser");
+
 const mongoose = require("mongoose");
 const budgetsModel = require("./models/budget_schema");
 
 let url = "mongodb://localhost:27017/budgets_db";
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/budget", (req, res) => {
   mongoose
     .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-      console.log("Connected to the database");
       budgetsModel
         .find({})
         .then((data) => {
@@ -29,6 +31,28 @@ app.get("/budget", (req, res) => {
     .catch((connectionError) => {
       console.log(connectionError);
     });
+});
+
+app.post("/add", (req, res) => {
+  mongoose
+    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      budgetsModel
+        .insertMany(req.body)
+        .then((data) => {
+          console.log(data);
+          res.json({ status: "success" });
+        })
+        .catch((conErr) => {
+          console.log(conErr);
+          res.json({ status: "db error" });
+        });
+    })
+    .catch((connectionError) => {
+      console.log(connectionError);
+      res.json({ status: "conn error" });
+    });
+    res.json({});
 });
 
 app.listen(port, () => {
